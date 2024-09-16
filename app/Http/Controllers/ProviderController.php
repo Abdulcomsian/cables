0c84449c6f936d41fcd8385e81e3b8739de6120a
naccrual <nauman.riaz@accrualhub.com>
2024-09-16 11:52:03 +0500

    filter issue

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -242,8 +242,7 @@ class ProviderController extends Controller
         }
 
         if(in_array('not required' , $offers)){
-          // $query1->where('line_rental' , 'not required');
-          $query1->whereNotNull('offer_ends')->where(DB::raw("TRIM(offer_ends)") , '!=' , '');
+          $query1->whereNotNull('offer_ends')->where(DB::raw("TRIM(offer_ends)") , '!=' , '')->whereNotNull('promo_and_info')->where(DB::raw("TRIM(promo_and_info)") , '!=' , '');
         }
 
 
